462aca39c97ed8f6867762cdbf86ad7655eaa280
emad <razavioo@gmail.com>
2026-05-29 19:37:56 +0330

    bump: version 1.1.7 for release

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 """Crawlemoon - Advanced Web Crawling Platform."""
 
-__version__ = "1.1.5"
+__version__ = "1.1.7"
 
 # Re-export the exception hierarchy so callers can use ``from src import CrawlemoonError``
 from .exceptions import (  # noqa: F401
